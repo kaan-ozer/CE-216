10e30fe0b04312ffacad2636e97c4239bf576c0b
kaan-ozer <kaanozer35@gmail.com>
2024-04-06 18:54:44 +0300

    connections/Development
    App.java, Library.java, and IOoperations has solid connections anymore.
    
    IOoperations has been updated.
    
    Library.java started to use IOoperations to handle JSON operations.
    
    Gradle build issues and conflicts has been resolved.

--- app/src/main/java/ce216project/App.java
+++ app/src/main/java/ce216project/App.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import ce216project.controller.PageController;
 import ce216project.models.Book;
+import ce216project.models.Library;
+import ce216project.utils.IOoperations;
 import ce216project.view.MainPage;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -19,19 +21,20 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        MainPage mainPage = new MainPage();
-        
-        ArrayList<Book> books = new ArrayList<>();
-        
-        
-        Book book1 = new Book("TestTitle", "some subtitle", new ArrayList<String>(), new ArrayList<String>(), "12312312312", "testPublisher", "12.02.2001", 2, "STYLESHEET_MODENA", "STYLESHEET_CASPIAN", 3, new ArrayList<String>(), null);
-       
 
-        for(int i=0; i < 200 ; i++){
-            books.add(book1);
-        }
 
-        mainPage.fillBookTiles(books);
+            IOoperations.resetJsonFile("output/hey.txt");
+            
+            MainPage mainPage = new MainPage();
+            
+            Book book1 = new Book("TestTitle", "some subtitle", new ArrayList<String>(), new ArrayList<String>(), "12312312312", "testPublisher", "12.02.2001", 2, "STYLESHEET_MODENA", "STYLESHEET_CASPIAN", 3, new ArrayList<String>(), null);
+            
+            
+            for(int i=0; i < 200 ; i++){
+                Library.createBook(book1); 
+            } 
+            
+        mainPage.fillBookTiles(Library.books);
         
         Scene scene = new Scene(mainPage,800,450);
         primaryStage.setScene(scene);
