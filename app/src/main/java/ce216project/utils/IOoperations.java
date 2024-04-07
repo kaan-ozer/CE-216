045e146ac5e851229ac2d4cf9ab61ee70355d5e6
Mert Çetin <mertctnstd35gmail.com>
2024-04-07 15:28:04 +0300

    I deleted main method in IOoperations class which is created for testing.

--- app/src/main/java/ce216project/utils/IOoperations.java
+++ app/src/main/java/ce216project/utils/IOoperations.java
@@ -41,55 +41,5 @@ public class IOoperations {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) {
-        List<String> authors1 = List.of("Author1", "Author2");
-        List<String> translators1 = List.of("Translator1", "Translator2");
-        List<String> tags1 = List.of("Tag1", "Tag2");
-
-        Book book1 = new Book(
-                "Title1",
-                "Subtitle1",
-                authors1,
-                translators1,
-                "ISBN1234",
-                "Publisher1",
-                new Date(),
-                1,
-                "Cover1",
-                "Language1",
-                4.5,
-                tags1
-        );
-
-        List<String> authors2 = List.of("Author3", "Author4");
-        List<String> translators2 = List.of("Translator3", "Translator4");
-        List<String> tags2 = List.of("Tag3", "Tag4");
-
-        Book book2 = new Book(
-                "Title2",
-                "Subtitle2",
-                authors2,
-                translators2,
-                "ISBN5678",
-                "Publisher2",
-                new Date(),
-                2,
-                "Cover2",
-                "Language2",
-                3.5,
-                tags2
-        );
-
-        List<Book> books = List.of(book1, book2);
-
-
-        //writeToJsonFile("example.json", books);
-
-
-        appendToJsonFile("example.json", books);
-
-
-        //resetJsonFile("example.json");
-    }
+    
 }
