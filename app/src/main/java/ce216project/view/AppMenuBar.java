20492a5dfd084dbe558bd8ebfe4963abb19e1e42
Sidelya <64807620+SidelyaCaliskan@users.noreply.github.com>
2024-04-27 11:43:11 +0300

    user guide added

--- app/src/main/java/ce216project/view/AppMenuBar.java
+++ app/src/main/java/ce216project/view/AppMenuBar.java
@@ -1,5 +1,11 @@
 package ce216project.view;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -8,22 +14,20 @@ import javafx.scene.input.KeyCombination;
 
 public class AppMenuBar extends MenuBar {
 
-    // Menu
     private Menu fileMenu = new Menu("File");
     private Menu helpMenu = new Menu("Help");
 
-    // File Menu Items
     private MenuItem mNewBook = new MenuItem("New"); // Ctrl+N
     private MenuItem mImport = new MenuItem("Import"); // Ctrl+O
     private MenuItem mExport = new MenuItem("Export"); // Ctrl+E
     private SeparatorMenuItem mFileSeparator = new SeparatorMenuItem();
     private MenuItem mQuit = new MenuItem("Quit");
 
-    // Help Menu Items
     private MenuItem mUserGuide = new MenuItem("User Guide"); // Ctrl+H
     private MenuItem mAbout = new MenuItem("About"); // Ctrl+A
     private SeparatorMenuItem mHelpSeparator = new SeparatorMenuItem();
-    private MenuItem mCheckForUpdates = new MenuItem("Check for Updates"); // Ctrl+U
+    // private MenuItem mCheckForUpdates = new MenuItem("Check for Updates"); //
+    // Ctrl+U
 
     public AppMenuBar() {
 
@@ -33,7 +37,7 @@ public class AppMenuBar extends MenuBar {
 
         mUserGuide.setAccelerator(KeyCombination.keyCombination("Ctrl+H"));
         mAbout.setAccelerator(KeyCombination.keyCombination("Ctrl+A"));
-        mCheckForUpdates.setAccelerator(KeyCombination.keyCombination("Ctrl+U"));
+        // mCheckForUpdates.setAccelerator(KeyCombination.keyCombination("Ctrl+U"));
 
         mQuit.setOnAction(e -> quit());
 
@@ -41,24 +45,71 @@ public class AppMenuBar extends MenuBar {
 
         mUserGuide.setOnAction(e -> showUserGuide());
         mAbout.setOnAction(e -> showAboutInfo());
-        mCheckForUpdates.setOnAction(e -> checkForUpdates());
+        // mCheckForUpdates.setOnAction(e -> checkForUpdates());
 
-        helpMenu.getItems().addAll(mUserGuide, mHelpSeparator, mCheckForUpdates, mAbout);
+        helpMenu.getItems().addAll(mUserGuide, mHelpSeparator, mAbout);
 
         this.getMenus().addAll(fileMenu, helpMenu);
     }
 
     private void showUserGuide() {
-        // Logic to show user guide
+        try {
+            File userGuide = new File("src/main/resources/userGuide.pdf");
+            if (userGuide.exists()) {
+                Desktop.getDesktop().open(userGuide);
+            } else {
+                System.out.println("User guide not found!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void showAboutInfo() {
-        // Logic to display about information
+        JOptionPane.showMessageDialog(null, "YourApp v1.0\nDeveloped by My G.", "About YourApp",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
-    private void checkForUpdates() {
-        // Logic to check for software updates
-    }
+    /*
+     * private void checkForUpdates() {
+     * String updateUrl = "http://yourappserver.com/api/check-updates";
+     * try {
+     * 
+     * @SuppressWarnings("deprecation")
+     * URL url = new URL(updateUrl);
+     * HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+     * conn.setRequestMethod("GET");
+     * int responseCode = conn.getResponseCode();
+     * if (responseCode == HttpURLConnection.HTTP_OK) {
+     * BufferedReader in = new BufferedReader(new
+     * InputStreamReader(conn.getInputStream()));
+     * String inputLine;
+     * StringBuilder response = new StringBuilder();
+     * while ((inputLine = in.readLine()) != null) {
+     * response.append(inputLine);
+     * }
+     * in.close();
+     * 
+     * String currentVersion = "1.0";
+     * String latestVersion = response.toString();
+     * if (!currentVersion.equals(latestVersion)) {
+     * 
+     * JOptionPane.showMessageDialog(null, "A new version is available!",
+     * "Update Available",
+     * JOptionPane.INFORMATION_MESSAGE);
+     * } else {
+     * JOptionPane.showMessageDialog(null, "You have the latest version.",
+     * "No Updates",
+     * JOptionPane.INFORMATION_MESSAGE);
+     * }
+     * } else {
+     * System.out.println("No response from update server.");
+     * }
+     * } catch (IOException e) {
+     * e.printStackTrace();
+     * }
+     * }
+     */
 
     private void quit() {
         System.exit(0);
