9b31366765321412f7e4d35c9d055a98cdf7ab10
MertCtn <mertctnstd35@gmail.com>
2024-04-29 18:13:22 +0300

    Move search bar to top of page

--- app/src/main/java/ce216project/view/MainPage.java
+++ app/src/main/java/ce216project/view/MainPage.java
@@ -65,7 +65,7 @@ public class MainPage extends VBox {
         //Checkbox Lists at Left Container
         tagsList.setMinWidth(100);
         checkBoxListContainer.getChildren().addAll(tagsList, languagesList);
-        checkBoxListContainer.setPadding(new Insets(5, 0, 0, 5));
+        checkBoxListContainer.setPadding(new Insets(0, 0, 0, 5));
 
         // Book add button
         addBookButton.setPrefWidth(100);
@@ -86,7 +86,7 @@ public class MainPage extends VBox {
         booksContainer.setPadding(new Insets(10, 10, 10, 10));
 
         // Main Containers
-        rightContainer.getChildren().addAll(searchBarContainer, booksScroll);
+        rightContainer.getChildren().addAll(booksScroll);
         leftContainer.getChildren().addAll(checkBoxListContainer, addBookButtonBox);
         mainLayout.getChildren().addAll(leftContainer, rightContainer);
 
@@ -107,7 +107,7 @@ public class MainPage extends VBox {
         booksContainer.prefWidthProperty().bind(booksScroll.widthProperty()); // Bind width to ScrollPane width
         booksContainer.maxWidthProperty().bind(booksScroll.widthProperty());
 
-        this.getChildren().addAll(menuBar, mainLayout);
+        this.getChildren().addAll(menuBar, searchBarContainer, mainLayout);
 
         // Update Main Page
         this.fillBookTiles(Library.books);
